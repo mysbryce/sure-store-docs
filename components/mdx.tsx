684d055ddd69db4c315c1aac91a8eadf21d15960
mysbryce <fx.frame009@gmail.com>
2026-04-02 00:39:31 +0700

    docs: reorganize es_extended documentation

--- components/mdx.tsx
+++ components/mdx.tsx
@@ -1,9 +1,15 @@
 import defaultMdxComponents from 'fumadocs-ui/mdx';
+import * as AccordionComponents from 'fumadocs-ui/components/accordion';
+import * as StepsComponents from 'fumadocs-ui/components/steps';
+import * as TabsComponents from 'fumadocs-ui/components/tabs';
 import type { MDXComponents } from 'mdx/types';
 
 export function getMDXComponents(components?: MDXComponents) {
   return {
     ...defaultMdxComponents,
+    ...AccordionComponents,
+    ...StepsComponents,
+    ...TabsComponents,
     ...components,
   } satisfies MDXComponents;
 }
